086d16b5868de738d072638f30e04530cf6f5457
CaroCald <carolina.2594@hotmail.com>
2018-04-18 15:39:48 -0500

    funciones

--- variables.js
+++ variables.js
@@ -6,7 +6,7 @@ var fechaNacimiento = new Date();
 var casado = false;
 var estoyVacio = null;
 var noEstoyDefinido = undefined;
-if (estoyVacio) {
+/*if (estoyVacio) {
     console.log("Verdadero")
 } else {
     console.log("Falso")
@@ -32,12 +32,45 @@ if (-1) {
 } else {
     console.log("Falso")
 }//verdadero
-
+*/
 /* Json * ctrl +a , ctrl +alt+l=formatear*/
 var usuario = {
     "nombre": "Carolina",
     apellido: 'Calderon',
-    edad: 24
+    edad: 24,
+    //utilizar funcion anonima
+    imprimirEnConsola: function () {
+        console.log(this.nombre+ ' '+ this.apellido+' '+ this.edad)
+    }
 };
+usuario.imprimirEnConsola();
 console.log(usuario.nombre);
 console.log(usuario);
+//se puede ir cambiando los atributos
+delete usuario.edad;
+console.log(usuario);
+// agregar atributos
+usuario.cedula= 1727152344;
+console.log(usuario);
+//no se puede definir nombre en undefined
+//usuario.mascotas.nombre='loki';
+// undefined porque mascotas no existe.
+//
+usuario.mascotas={};
+usuario.mascotas.nombre='loki';
+console.log(usuario);
+//type of
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof deudas);
+console.log(typeof casado);
+//funciones
+function sumarDosNumeros( numeroUno, numeroDos) {
+    return numeroUno+numeroDos;
+}
+console.log(sumarDosNumeros(1,2));
+// no se necesita escribir el nombre de la funcion--> FUNCION ANONIMA
+var sumarDosNumerosVersionDos = function( numeroUno, numeroDos) {
+    return numeroUno+numeroDos;
+}
+console.log(sumarDosNumerosVersionDos(1,3));
